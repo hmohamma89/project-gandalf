48fc59fd330bfc7da5031377e9d3f87abdb51448
WIPCORE\hamdoon.mohammad <hamdoon.mohammad@wipcore.se>
2017-11-07 14:28:57 +0100

    test 16

--- scripts/pre-push.sh
+++ scripts/pre-push.sh
@@ -1,5 +1,9 @@
 #!/bin/bash 
 
+SOLUTIONPATH="D:\git-hooks-tests"
+TESTFILESTOEXCLUDE="Unit2Tests"
+MSTEST="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe"
+
 protected_branch='master'
 # Check if we actually have commits to push
 commits=`git log @{u}..`
@@ -10,13 +14,16 @@ fi
 current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
 
 if [[ $current_branch = $protected_branch ]]; then
+    echo "Solutions path: $SOLUTIONPATH"
+    echo "Test Files to exclude: $TESTFILESTOEXCLUDE"
+    echo "Mstest path: $MSTEST"
 	echo "Runing unit tests"
     # Command that runs your tests
-    c:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -File .\\scripts\\run-tests.ps1 -solutionPath "D:\git-hooks-tests" -testFilesToExclude "Unit2Tests" -mstest "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\Common7\IDE\MSTest.exe" 
+    c:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -File .\\scripts\\run-tests.ps1 -solutionPath SOLUTIONPATH -testFilesToExclude TESTFILESTOEXCLUDE -mstest MSTEST
     RESULT=$?
     echo  "the result is: $RESULT"
     if [ $RESULT -ne 0 ]; then 
-        echo "unit tests failed please fix them before you push to the git repository"
+        echo "Your unit tests are failing please fix them before you push to your git repository"
         exit 1
     fi
 fi
